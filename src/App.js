d8aa259975c3e32e939edc4f8c0d186e044db961
zaid <mohammedzaid.452@gmail.com>
2020-08-19 15:31:55 +0530

    json status

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
 class App extends Component {
   render() {
+
+
+    let url = 'https://api.covid19india.org/data.json';
+
+fetch(url)
+.then(res => res.json())
+.then((out) => {
+  console.log('Checkout this full data ! ', out);
+})
+.catch(err => { throw err });
+
+
+axios.get('https://api.covid19india.org/state_district_wise.json')
+  .then(function (response) {
+    console.log('checkout district wise data',response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
     return (
       <div className="App">
         <header className="App-header">
